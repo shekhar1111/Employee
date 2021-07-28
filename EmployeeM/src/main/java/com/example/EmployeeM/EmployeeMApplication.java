92521b75e9a6aaf4521d2fdf1cbbec74a5a305f7
shekhar <jundalesomshekhar@gmail.com>
2021-07-28 10:35:41 +0530

    initial commit

---
+++ EmployeeM/src/main/java/com/example/EmployeeM/EmployeeMApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMApplication.class, args);
+	}
+
+}
